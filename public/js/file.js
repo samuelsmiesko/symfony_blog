d2b18a1d81d0ff4835495f47907d8ab5d33687e2
samoprogramuje <samuel1820@gmail.com>
2024-04-13 06:42:09 +0200

    add styling

--- public/js/file.js
+++ public/js/file.js
@@ -1,7 +1,11 @@
+
+
+
+
 myFunction()
 
 function myFunction() {
-    console.log("myFunction");
+    
     const csslink = document.querySelector("#myImg");
     if (localStorage.getItem("variable") === "white") {
         localStorage.setItem("variable", "black");
