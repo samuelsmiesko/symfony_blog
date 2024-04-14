9818ef82083762d5bb1d249a9077a0b705abf4bb
samoprogramuje <samuel1820@gmail.com>
2024-04-14 14:54:11 +0200

    add paging

--- public/js/file.js
+++ public/js/file.js
@@ -1,21 +1,52 @@
 
 
 
+    
+const csslink = document.querySelector("#myImg");
+if (localStorage.getItem("variable") === "white") {
+    document.querySelectorAll(".paginationTag").forEach((el) => {
+        el.classList.add('bg-dark');
+        el.classList.remove('bg-white');
+    });   
+    csslink.setAttribute("href", "../css/dark.css"); 
 
-myFunction()
+} else {
+    document.querySelectorAll(".paginationTag").forEach((el) => {
+        el.classList.add('bg-white');
+        el.classList.remove('bg-dark');
+    });
 
-function myFunction() {
+    csslink.setAttribute("href", "../css/light.css");
+}
+
+
+
+document.getElementById("lightDark").addEventListener("click",function(event){
+    event.preventDefault();
+    /* your code here */
     
     const csslink = document.querySelector("#myImg");
+    
     if (localStorage.getItem("variable") === "white") {
+        
         localStorage.setItem("variable", "black");
+        document.querySelectorAll(".paginationTag").forEach((el) => {
+            el.classList.add('bg-white');
+            el.classList.remove('bg-dark');
+        });
         
         csslink.setAttribute("href", "../css/light.css"); 
 
         
     } else {
+        
         localStorage.setItem("variable", "white");
+        document.querySelectorAll(".paginationTag").forEach((el) => {
+            el.classList.add('bg-dark');
+            el.classList.remove('bg-white');
+        });
         
         csslink.setAttribute("href", "../css/dark.css");
+        
     }
-}
+});
