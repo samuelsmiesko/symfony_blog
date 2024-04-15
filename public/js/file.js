807f0ba2919093d926e4d7a05144b8a8d8125ff4
samoprogramuje <samuel1820@gmail.com>
2024-04-15 17:36:53 +0200

    befour jqurry

--- public/js/file.js
+++ public/js/file.js
@@ -1,4 +1,4 @@
-
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
 
 
     
@@ -49,4 +49,11 @@ document.getElementById("lightDark").addEventListener("click",function(event){
         csslink.setAttribute("href", "../css/dark.css");
         
     }
+});
+
+jQuery(document).ready(function() {
+    var searchRequest = null;
+    $("#search").keyup(function() {
+        
+    });
 });
