c99557a276aee5c07a71db230aed2356d0e95a22
samoprogramuje <samuel1820@gmail.com>
2024-05-06 12:56:56 +0200

    befour relations

---
+++ migrations/Version20240505164210.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240505164210 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE blogs (id INT AUTO_INCREMENT NOT NULL, title LONGTEXT NOT NULL, article LONGTEXT NOT NULL, image LONGBLOB NOT NULL, author VARCHAR(255) NOT NULL, priority INT NOT NULL, imageaddress VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE comments (id INT AUTO_INCREMENT NOT NULL, article LONGTEXT NOT NULL, author VARCHAR(255) NOT NULL, date VARCHAR(255) NOT NULL, commentsid LONGTEXT NOT NULL, commentslikes LONGTEXT DEFAULT NULL, commentsdislikes LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reactions (id INT AUTO_INCREMENT NOT NULL, article LONGTEXT NOT NULL, author VARCHAR(255) NOT NULL, date VARCHAR(255) NOT NULL, comments_id LONGTEXT NOT NULL, commentslikes LONGTEXT DEFAULT NULL, commentsdislikes LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE blogs');
+        $this->addSql('DROP TABLE comments');
+        $this->addSql('DROP TABLE reactions');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE users');
+    }
+}
